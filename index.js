9673ac0a7bdf359f92a8bd7079dc795a2a4d1a1f
Hon Lam <hon@junglescout.com>
2019-01-14 23:22:10 -0800

    Test Dropbox token variable.

--- index.js
+++ index.js
@@ -7,5 +7,10 @@ console.log(exportTest)
 console.log("I am from index also")
 console.log(process.env.PRINT_ME)
 
+console.log("should be token")
+console.log(process.env.DROPBOX_ACCOUNT_ACCESS_TOKEN)
+
 // let element = document.querySelector("#container")
 // element.innerHTML = "hello from index.js. hooray! something"
+
+const res = await 

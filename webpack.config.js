907ce5fec910878d3ba4f00c8aa9c1302dd9002c
Hon Lam <hon@junglescout.com>
2019-01-27 17:19:19 -0800

    Update webpack config from starter repo. Resolve its conflicts with Express.

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,31 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
 
   // IMPORTANT: Sort keys alphabetically
 
+  // Used for live reloading.
   devServer: {
+    // How the server should find the files.
     contentBase: './dist',
     hot: true
   },
+  devtool: 'inline-source-map',
   entry: './index.js',
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
   },
+  // Resolved conflicts between Webpack and Express.
+  node: {
+    fs: 'empty',
+    net: 'empty'
+  },
   output: {
+    // Generated in memory, not physical file.
+    // Has options to programmatically format filename.
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
